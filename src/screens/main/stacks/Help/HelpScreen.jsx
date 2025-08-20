830c6f65dc183a9b4ab606b7bbc2a77e6161ae14
SyedHamzaHussain000 <syedhamza0015@gmail.com>
2025-08-20 22:02:14 +0500

    fixed

--- src/screens/main/stacks/Help/HelpScreen.jsx
+++ src/screens/main/stacks/Help/HelpScreen.jsx
@@ -10,7 +10,7 @@ const HelpScreen = ({navigation}) => {
 
   const pollens = [
     {id: 1, name: 'View App Guide & Instruction',onPress: ()=> navigation.navigate("ViewAppGuide"), top: true, },
-    // {id: 2, name: 'View Premium App Guide', },
+    {id: 2, name: 'View Premium App Guide', },
     {id: 2, name: 'Tips & Tricks', onPress: ()=> navigation.navigate("TipsTrick")},
     {id: 3, name: 'Pollen Information for Canada', bottom: true, onPress: ()=> navigation.navigate("PollenInfoForCad")},
   ];
