b836db8389e221ae3575ba44adb754f0c7b63993
SyedHamzaHussain000 <syedhamza0015@gmail.com>
2025-09-09 04:30:04 +0500

    completted

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,19 +1,43 @@
 import {View, Text} from 'react-native';
-import React from 'react';
+import React, { useEffect } from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
 import Auth from './Auth';
 import Main from './Main';
-import { useSelector } from 'react-redux';
-
+import { useDispatch, useSelector } from 'react-redux';
+import NetInfo from '@react-native-community/netinfo'
+import { setInternet } from '../redux/Slices/BlackListSlice';
+import InternetConnection from '../screens/InternetConnection/InternetConnection';
 const Stack = createStackNavigator();
 const Routes = () => {
 
+  const internetConnection = useSelector(state => state?.blacklist?.isInternetConnected)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    const unsubscribe = NetInfo.addEventListener(state => {
+      dispatch(setInternet(state.isConnected))
+        console.log("is",state.isConnected)
+      // setIsConnected(state.isConnected);
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  }, []);
 
   return (
-    <Stack.Navigator initialRouteName='Auth'  screenOptions={{headerShown:false}}>
-      
-      <Stack.Screen name="Main" component={Main} />
-      <Stack.Screen name="Auth" component={Auth} />
+    <Stack.Navigator  screenOptions={{headerShown:false}}>
+      {
+        internetConnection == false ? (
+          <Stack.Screen name="InternetConnection" component={InternetConnection} />
+        )
+        :(
+          <>
+          <Stack.Screen name="Auth" component={Auth} />
+          <Stack.Screen name="Main" component={Main} />
+          </>
+        )
+      }
       
     </Stack.Navigator>
   );
